0eac36f4f469464ee590690a818b4d428a3678bd
Jason <78479629+imjason@users.noreply.github.com>
2024-05-09 09:52:22 +0000

    update

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,5 +2,14 @@
 export default defineNuxtConfig({
   devtools: { enabled: true },
   modules: ["@nuxtjs/tailwindcss"],
+  tailwindcss: {
+    cssPath: '~/assets/css/tailwind.css',
+    configPath: 'tailwind.config',
+    exposeConfig: false,
+    exposeLevel: 2,
+    config: {},
+    injectPosition: 'first',
+    viewer: true,
+  },
   plugins: ["~/plugins/preline.client.ts"]
 })
